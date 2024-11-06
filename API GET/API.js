47af06a41c23ea0bf48f56e28d8f7ae588af25cb
marvel22122 <marvelousademola5@gmail.com>
2024-11-06 13:29:42 +0100

    Add files via upload

---
+++ API GET/API.js
@@ -0,0 +1,54 @@
+async function getProduct() {
+  try {
+    const response = await fetch("https://fakestoreapi.com/products");
+
+    console.log("initial response:", response);
+
+    //convert the returned promise to a javascript object
+    const productData = await response.json();
+
+    console.log("productData:", productData);
+
+    //grab an empty container from the DOM
+    const productCard = document.getElementById("product - card");
+
+    //create a variable and initiates it with an empty string
+    let myCards = "";
+
+
+    //Loop through the array of productfetched
+    productData.map((item) => {
+      myCards += 
+      `
+             <div class="card">
+            <img src= ${item.image} />
+
+            <h3>tittle</h3>
+            <p>${item.price}</p>
+
+            <h3>${item.title}/h3>
+            <div class="amount">
+                <h5>Price:</h5>
+                <p>${item.price}</p>
+            </div>   
+
+            <div class="category">
+                <h5>Category:</h5>
+                <p>${item.category}</p>
+            </div>
+
+            <p>${item.description}</p>
+        </div>
+        `
+    });
+
+    productCard.innerHTML = myCards;
+
+  }
+   catch (error) {
+    console.log("product error:", error);
+}
+
+};
+
+getProduct();
